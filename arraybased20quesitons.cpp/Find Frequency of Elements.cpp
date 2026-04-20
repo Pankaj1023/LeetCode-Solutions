95a2a3b41e5e53d9a99b9ea87e224c89ae62a857
Pankaj Sharma <yourgithubemail@gmail.com>
2026-04-20 18:10:34 +0530

    Find Frequency of Elements

---
+++ arraybased20quesitons.cpp/Find Frequency of Elements.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;   // total size
+
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    cin>>a[i];     // total elements input
+
+    unordered_map<int,int>freq;   // key and elements store 
+
+    for(int i=0;i<n;i++)
+    {
+        freq[a[i]]++;
+
+    }
+
+    for(auto it: freq)
+    {
+    cout<<it:first<<"->"<<it.second<<"\n";
+    }
+    return 0;
+
+}
