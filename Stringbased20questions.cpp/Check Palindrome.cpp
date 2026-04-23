42ac4bf9001d728737e1b39d8137e212a0acdd63
Pankaj Sharma <yourgithubemail@gmail.com>
2026-04-23 17:55:49 +0530

    class Solution {

---
+++ Stringbased20questions.cpp/Check Palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+
+        while(i < j) {
+            if(s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
