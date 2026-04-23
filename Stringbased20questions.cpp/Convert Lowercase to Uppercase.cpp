8e8b351869ea165f65f8b71219558b9ab8beaf1f
Pankaj Sharma <yourgithubemail@gmail.com>
2026-04-23 18:36:55 +0530

    Convert Lowercase to Uppercase

---
+++ Stringbased20questions.cpp/Convert Lowercase to Uppercase.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+
+    for(int i=0;i<s.lenght();i++)
+    {
+        s[i]=toupper(s[i]);
+    }
+    cout<<s;
+    return 0;
+
+}
