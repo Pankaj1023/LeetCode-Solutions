282df778ca69a569b95de8e157761e20348f4d87
Pankaj Sharma <yourgithubemail@gmail.com>
2026-04-26 18:00:48 +0530

    Sort Characters in String

--- Stringbased20questions.cpp/Remove Duplicate Characters.cpp
+++ Stringbased20questions.cpp/Remove Duplicate Characters.cpp
@@ -5,15 +5,11 @@ int main()
     string s;
     cin>>s;
 
-    char ch;
-    cin>>ch;
-
-    int count=0;
+    string result ="";
 
     for(int i=0;i<s.length();i++)
     {
-        if(s[i]==ch)
-        count++;
+        boolisDuplicate
+
     }
-    cout<<count<<"\n";
 }
