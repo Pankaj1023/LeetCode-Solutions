b17d39a20c49a6ca135e84a1b3bd442235fd0199
Pankaj Sharma <yourgithubemail@gmail.com>
2026-04-28 18:53:13 +0530

    1️⃣9️⃣ Check if Two Strings are Equal

---
+++ Stringbased20questions.cpp/1️⃣9️⃣ Check if Two Strings are Equal.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    if(s1 == s2)
+        cout << "Equal";
+    else
+        cout << "Not Equal";
+
+    return 0;
+}
