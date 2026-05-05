9e0d0836fbbf1ce97c381183bbb1691a7454f8c3
Pankaj Sharma <yourgithubemail@gmail.com>
2026-05-05 19:00:52 +0530

    5️⃣ Sum of First N Numbers

---
+++ 20 RECURSION QUESTIONS/5️⃣ Sum of First N Numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumN(int n) {
+        return n*(n+1)/2;
+    }
+}
+
+
+//   Approach 2 (Recursion)
+
+class Solution {
+public:
+    int sumN(int n) {
+        if(n==0) return n;
+
+        return n+sumN(n-1);
+    }
+}
