9008ee1170a8e62895fa84f920d606b2f54516de
Pankaj Sharma <yourgithubemail@gmail.com>
2026-05-11 19:58:45 +0530

    1️⃣6️⃣ Staircase Problem ⭐🔥

---
+++ 20 RECURSION QUESTIONS/1️⃣6️⃣ Staircase Problem ⭐🔥 .cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+
+    int climbStairs(int n) {
+
+        // base case
+        if(n == 0 || n == 1)
+            return 1;
+
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+};
